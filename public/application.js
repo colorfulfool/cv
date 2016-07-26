cdd5dce54aff2d856eefbc58a2e2f61671a8d6ef
colorfulfool <t0ny.hamster@gmail.com>
2016-07-26 14:48:49 +0300

    Revamp the mobile layout

--- public/application.js
+++ public/application.js
@@ -2,7 +2,7 @@ $(function (){
   $('[scroll-to]').click(function () {
     scroll_target = $(this).attr('scroll-to');
     $('html,body').animate({
-      scrollTop: $(scroll_target).offset().top - 27*5
+      scrollTop: $(scroll_target).offset().top - 27*3
     })
   })
   return false;
